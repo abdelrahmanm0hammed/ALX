f4f25e7aa660317317a5d07eded326a354a9e984
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-01-15 22:35:08 +0200

    created a userprofile model with a role and user fields

--- week10/LibraryProject/relationship_app/models.py
+++ week10/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Author(models.Model):
@@ -16,3 +17,15 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    ROLE_CHOICES ={ "ADMIN":"Admin",
+        "LIBRARIAN":"Librarian",
+        "MEMBER":"Member"}
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(choices=ROLE_CHOICES,default='MEMBER'
+    )
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+        
