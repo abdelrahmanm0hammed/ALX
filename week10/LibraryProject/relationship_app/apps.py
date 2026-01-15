268d51faf27fac90d0ab260583280f2c05784e59
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-01-15 23:03:55 +0200

    creating signals.py to create a userprofile whenever a user is created

--- week10/LibraryProject/relationship_app/apps.py
+++ week10/LibraryProject/relationship_app/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class RelationshipAppConfig(AppConfig):
     name = 'relationship_app'
+    default_auto_field = 'django.db.models.BigAutoField'
+
+    def ready(self):
+        import relationship_app.signals
