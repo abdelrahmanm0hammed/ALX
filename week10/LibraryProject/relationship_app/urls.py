02e97cdf1654713d1719483395339b6a61546fa8
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-01-17 22:00:45 +0200

    creating add_book, change_book and delete_book views and urls

--- week10/LibraryProject/relationship_app/urls.py
+++ week10/LibraryProject/relationship_app/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LoginView.as_view(template_name='relationship_app/logout.html')),
-    path('register/',views.SignUp.as_view(), name='register')
+    path('register/',views.SignUp.as_view(), name='register'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add_book/',views.add_book, name='add_book'),
+    path('change_book/<int:pk>/',views.change_book, name='change_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name= 'delete_book'),
 ]
--- week10/LibraryProject/relationship_app/views.py
+++ week10/LibraryProject/relationship_app/views.py
@@ -4,7 +4,9 @@ from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import  redirect, get_object_or_404
 
 
 
@@ -21,3 +23,58 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'ADMIN'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'LIBRARIAN'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'MEMBER'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+@login_required
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
+        Book.objects.create(title=title, author=author, publication_year= publication_year)
+        return redirect('list_books')
+    return render(request, 'relationship_app/add_book.html')
+
+@login_required
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def change_book(request, pk):
+    book = get_object_or_404(Book , pk=pk)
+    if request.method == "POST":
+        book.title =request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        return redirect('list_books')
+    return render (request, 'relationship_app/change_book.html',{'book':book})
+
+@login_required
+@permission_required('relationship_app.can_delete.book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html',{'book':book})
