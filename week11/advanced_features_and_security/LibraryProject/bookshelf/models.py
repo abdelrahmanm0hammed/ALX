6e7f98bc9902581033a2e14a25bf8ecbf9abc23d
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-01-19 21:44:52 +0200

    creating custom permissions : add, view, edit and delete to book model

--- week11/advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ week11/advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -4,5 +4,12 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    class Meta:
+        permissions = [
+            ("can_view", "can view book"),
+            ("can_create", "can create book"),
+            ("can_edit", "can edit book"),
+            ("can_delete","can delete book")
+        ]
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author} in {self.publication_year}"
