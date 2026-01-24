31051d810b275123ae6b8c0c3d706a7870997bb2
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-01-24 21:51:43 +0200

    Implementing authentication and permissions in Django rest framework

--- week12/api_project/api/views.py
+++ week12/api_project/api/views.py
@@ -3,10 +3,15 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+#this viewset uses TokenAuthentication
+#Only logged-in users with a valid token can acess it 
+#Token must be sent in Authorization header
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
--- week12/api_project/api_project/settings.py
+++ week12/api_project/api_project/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": 
                   ("rest_framework.authentication.TokenAuthentication",
                    "rest_framework.authentication.SessionAuthentication",),
+                   'DEFAULT_PERMISSION_CLASSES': [
+                       'rest_framework.permissions.AllowAny',
+                   ]
+
+
+                   
                    }
 
 
