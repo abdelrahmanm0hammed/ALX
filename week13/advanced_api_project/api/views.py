cfad07833ce4d2fbc4ef8c139cf5e6ff47f94fbc
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-01-27 17:03:31 +0200

    setting up filtering functionality to booklist view

--- week13/advanced_api_project/api/views.py
+++ week13/advanced_api_project/api/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
 
 
 class BookDetailView(generics.RetrieveAPIView):
